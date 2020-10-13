e3361ecfcef13e3d936a6fb41db1ea36ef83c9ae
Franklin Pugh <fdp8383@ad.rit.edu>
2020-10-13 12:10:47 -0400

    Added all the pieces to an array within the board script
    In my opinion the code looks bad and I am sure there's a better way to do this, but for now, this will work.

--- Assets/Scripts/BoardScript.cs
+++ Assets/Scripts/BoardScript.cs
@@ -6,7 +6,6 @@ using UnityEngine;
 
 public class BoardScript : MonoBehaviour
 {
-
     //dimensions of board
     [SerializeField]
     private int xSpaces = 0;
@@ -16,6 +15,8 @@ public class BoardScript : MonoBehaviour
     private GameObject square; //gridsquare
 
     private Vector2[,] points; //all of the grid points
+
+    public GameObject[] pieces; //all of the pieces
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +34,19 @@ public class BoardScript : MonoBehaviour
                 Instantiate(square, new Vector3(points[i, n].x, points[i, n].y, 0), Quaternion.identity).transform.parent = transform; //draw a grid square around the point and set the parent to the board
             }
         }
+
+        //initialize and add the pieces to the pieces array
+        pieces = new GameObject[10];
+        pieces[0] = GameObject.Find("SquareBlock");
+        pieces[1] = GameObject.Find("UBlock");
+        pieces[2] = GameObject.Find("ZigZagBlock");
+        pieces[3] = GameObject.Find("CrossBlock");
+        pieces[4] = GameObject.Find("ZBlock");
+        pieces[5] = GameObject.Find("StraightBlock");
+        pieces[6] = GameObject.Find("AxeBlock");
+        pieces[7] = GameObject.Find("SmallLBlock");
+        pieces[8] = GameObject.Find("FlippedHammerBlock");
+        pieces[9] = GameObject.Find("FlippedLBlock");
     }
 
     //get the nearest point on the grid to a given point. will use for snapping pieces to grid
