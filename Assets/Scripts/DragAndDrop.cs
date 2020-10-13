ca4aca3d9911ae96d2e4512106f4cacc54aac3e6
jrs3462 <jrs3462@g.rit.edu>
2020-10-13 00:16:28 -0400

    DragAndDrop
    Drag and drop now works

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,18 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DragAndDrop : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private float startPosX;
+    private float startPosY;
+    private bool held = false;
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
+<<<<<<< Updated upstream
 
+=======
+        if (held == true)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, mousePos.y - startPosY, 0);
+        }
+>>>>>>> Stashed changes
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            held = true;
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+        }
+    }
+
+    private void OnMouseUp()
+    {
+<<<<<<< Updated upstream
+
+=======
+        held = false;
+>>>>>>> Stashed changes
     }
 }
+
