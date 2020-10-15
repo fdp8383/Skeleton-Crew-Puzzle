5492ca92703721b4d6921f65f94bba2c48ba4891
Tony Weng <46794056+tony-weng@users.noreply.github.com>
2020-10-15 19:03:39 -0400

    FInished Hotbar button script
    Button script now pushses out debug info into the console. Will need to replace this with the prefabs once I learn how to spawn based off the resoucres folder in code.

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -1,28 +1,36 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class Block : MonoBehaviour
 {
+    [SerializeField] private TMP_Text _text;
+
     public event Action<int> OnButtonClicked;
 
+    private KeyCode _keyCode;
+    private int _keyNumber;
+    
+    private void OnValidate()
+    {
+        _keyNumber = transform.GetSiblingIndex() + 1;
+        _keyCode = KeyCode.Alpha0 + _keyNumber;
+        if (_text == null)
+            _text = GetComponentInChildren<TMP_Text>();
+        _text.SetText(_keyNumber.ToString());
+        gameObject.name = "Hotbar Button " + _keyNumber;
+    }
     private void Awake()
     {
-        //GetComponent<Button>().onClick.AddListener(HandleClick);
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        GetComponent<Button>().onClick.AddListener(HandleClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void HandleClick()
     {
-    
+        OnButtonClicked?.Invoke(_keyNumber);
     }
 
-    //private void HandleClick
 }
