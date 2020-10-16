9bfe857d5f4e98734ea167a47077875cfbaf7fac
Tony Weng <46794056+tony-weng@users.noreply.github.com>
2020-10-16 15:21:56 -0400

    Got spawning to work for Hotbar
    Hotbar currently spawns pieces based off the location of the mouse but you are not able to drag and drop them right away. I believe this to be the fault of the OnClick method as it activates once immediate and cant be continous with OnMouseDown.

--- Assets/Scripts/BlockManager.cs
+++ Assets/Scripts/BlockManager.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 
 public class BlockManager : MonoBehaviour
 {
+    public GameObject[] pieces; //all of the pieces
+
     private void Awake()
     {
         foreach(var button in  GetComponentsInChildren<Block>())
@@ -14,9 +16,26 @@ public class BlockManager : MonoBehaviour
         }
         
     }
-    
+
+    void Start()
+    {
+        pieces = new GameObject[]
+       {
+            Resources.Load<GameObject>("AxeBlock"),
+            Resources.Load<GameObject>("CrossBlock"),
+            Resources.Load<GameObject>("FlippedHammerBlock"),
+            Resources.Load<GameObject>("SmallLBlock"),
+            Resources.Load<GameObject>("SquareBlock"),
+            Resources.Load<GameObject>("StraightBlock"),
+            Resources.Load<GameObject>("UBlock"),
+            Resources.Load<GameObject>("ZBlock"),
+            Resources.Load<GameObject>("ZigZagBlock")
+       };
+    }
     private void ButtonOnOnButtonClicked(int buttonNumber)
     {
         Debug.Log(message: $"Button {buttonNumber} clicked! ");
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition); //sets mousePos according to screen
+        Instantiate(pieces[buttonNumber], new Vector3(mousePos.x,mousePos.y, 0), Quaternion.identity);
     }
 }
